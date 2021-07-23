fe79a7074a8b0163ecde2d4e5ccb7449e14cf05b
ABHIRUP BASU <69730155+basuabhirup@users.noreply.github.com>
2021-07-23 11:34:59 +0530

    Update index.js
    Fix hour-hand & minute-hand's smooth rotation issue + minor revisions

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// Setting an interval of 1 seconds to call a custom function repetitively:
+// Setting an interval of 1 seconds to call a custom function periodically:
 setInterval (changeTime, 1000);
 
 // Defining the custom changeTime function:
 function changeTime() {
-  var timeNow = new Date();
+  
+  var timeNow = new Date(); // new Date object to store the current date & time
 
   var seconds = timeNow.getSeconds(); // get seconds from the new Date object
   var secondsInDegrees = (seconds / 60) * 360;
   var secondsHand = document.querySelector(".second-hand");
   secondsHand.style.transform = "rotate(" + (secondsInDegrees + 90) + "deg)";
-  // Extra 90 degrees addition to consider rotation from the initial position i.e. -90deg
+  // extra 90 degrees addition to consider rotation from the initial position i.e. -90deg
 
   var mins = timeNow.getMinutes(); // get minutes from the new Date object
-  var minsInDegrees = (mins / 60) * 360;
+  var minsInDegrees = (mins / 60) * 360 + (secondsInDegrees / 60);
   var minsHand = document.querySelector(".min-hand");
   minsHand.style.transform = "rotate(" + (minsInDegrees + 90) + "deg)";
 
   var hours = timeNow.getHours(); // get hours from the new Date object
-  var hoursInDegrees = (hours / 12) * 360;
+  var hoursInDegrees = (hours / 12) * 360 + (minsInDegrees / 12);
   var hoursHand = document.querySelector(".hour-hand");
   hoursHand.style.transform = "rotate(" + (hoursInDegrees + 90) + "deg)";
-
-  console.log (hours, mins, seconds);
-
+  
+  // digits to be displayed on the digital clock:
   secondsInTwoDigits = twoDigits(seconds); // calls a custom function twoDigits
   minsInTwoDigits = twoDigits(mins);
   hoursInTwoDigits = twoDigits(hours);
@@ -34,7 +34,12 @@ function changeTime() {
       return x;
     }
   }
+  
   var digitalTime = hoursInTwoDigits + "h " + minsInTwoDigits + "m " + secondsInTwoDigits + "s"
   var digitalTimeObject = document.querySelector(".digital-time");
   digitalTimeObject.textContent = digitalTime;
+  
 }
+
+// Calling the changeTime function on loading of the page:
+changeTime();
